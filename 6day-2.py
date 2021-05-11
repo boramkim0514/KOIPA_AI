cc1e16144300c893040bebc06521864ce03d76b1
boramkim0514 <83566064+boramkim0514@users.noreply.github.com>
2021-05-11 16:26:33 +0900

    Add files via upload

---
+++ 6day-2.py
@@ -0,0 +1,36 @@
+
+# print('start')
+# try: 
+#     2 / 0
+# except ZeroDivisionError as msg :
+#         print(msg)
+# print('end')
+
+a = [1, 2, 3]
+# except ZezoDivisionError as msg:
+#print(msg)
+try : 
+    a[3] = 4
+    "korea" + 4
+except IndexError as msg :
+    print(msg)
+
+
+
+except(IndextError, TypeError, ZerodivisionError):
+    
+else : 
+     print('IndexErorr, TypeError, ZeroDivisionError 아니다.')
+
+finally :
+    print('에러와 상관없이 무조건 실행.. ')
+
+print('** end **')
+
+    
+    ), Type Error 
+    print(' tptwnd gksk...)')
+except TypeError as msg:
+selsepprint('end')
+
+finally: 
